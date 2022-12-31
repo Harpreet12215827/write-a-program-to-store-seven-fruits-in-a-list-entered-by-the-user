fc08aa563fee76952646ca9db3f9782a07621fae
Harpreetsingh12215827 <119067585+Harpreet12215827@users.noreply.github.com>
2022-12-31 16:36:50 +0530

    Add files via upload
    Write a program to store seven fruits in a list entered by the user

---
+++ Write a Program To Store Seven fruits in a list entered by the User.py
@@ -0,0 +1,11 @@
+#write a program to store seven fruits in a list entered by the user.
+
+a = input("Name of 1st Fruit:  ")
+b = input("Name of 2nd Fruit:  ")
+c = input("Name of 3rd Fruit:  ")
+d = input("Name of 4th Fruit:  ")
+e = input("Name of 5th Fruit:  ")
+f = input("Name of 6th Fruit:  ")
+g = input("Name of 7th Fruit:  ")
+M = list(a,b,c,d,e,f,g)
+print(M)
